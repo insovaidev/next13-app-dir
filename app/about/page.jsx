ec3c864a10114443ec88aa23e6b75a22b75017ac
insovaideveloper <insovaidev@gmail.com>
2023-05-06 14:30:12 +0700

    first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const metadata = {
+    title: 'About My App'
+}
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About My App</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+        fuga minima similique molestias dignissimos aspernatur eveniet delectus
+        est obcaecati labore! Ad inventore nobis praesentium tempore cupiditate!
+        Libero facere ratione nihil dolore eaque iste saepe quos suscipit, odio,
+        perspiciatis rerum tempore amet impedit. Debitis porro facere asperiores
+        officia iusto laborum assumenda.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel in, omnis
+        veritatis, quasi distinctio repellat ipsam pariatur fugiat eius esse ex
+        maxime magni deleniti officiis sunt minus, ducimus itaque consequuntur
+        tempora sint officia blanditiis! Eius dolores repellat obcaecati ullam,
+        similique recusandae ipsum optio veniam et nemo iusto nisi reprehenderit
+        distinctio?
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
